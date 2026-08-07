7b4231b9aca5604363b85b2aa3e54df34b902e05
towsifislambiz <towsifislambiz@gmail.com>
2026-08-08 04:49:09 +0600

    feat  : add all done

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyB1rwKUX26xmyNvoZscYW5eGOrOE6JB5ys",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "expense-tracker-c8331.firebaseapp.com",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "expense-tracker-c8331",
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "expense-tracker-c8331.firebasestorage.app",
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "889683600097",
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:889683600097:web:912ccb8776f278e46c8da0",
 };
 
 const app = initializeApp(firebaseConfig);
